5a971cadee7860318a0d139701c24581a691a322
Emmanuel Cortes <polosetroc@gmail.com>
2020-07-18 09:18:30 -0700

    Update Request.java

--- src/com/manny/smmry/Request.java
+++ src/com/manny/smmry/Request.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.io.*;
 import java.util.HashMap;
 
+/**
+Generates url string for API call, creates connections, parses results and returns a formatted JSON string
+**/
 public class Request {
     private static String baseURL = "http://api.smmry.com/";
     private static String APIKeyLink = "&SM_API_KEY=N"; // Mandatory, N represents your registered API key.
@@ -14,7 +17,22 @@ public class Request {
     private static String APIRequestNoQuote = "&SM_QUOTE_AVOID"; // Optional, summary will not include quotations
     private static String APIRequestBreak = "&SM_WITH_BREAK"; // Optional, summary will contain string [BREAK] between each sentence
 
-    //Make an API call to SUMMRY and returns it response in a formatted json string.
+    //Makes url string for api call
+    private static String urlBuilder ():
+        String strSmmry = baseURL + APIKeyLink;
+            if (requestKeywordCount != null)
+                strSmmry += APIRequestKeywordCount + requestKeywordCount;
+            if (requestLength != null)
+                strSmmry += APIRequestLength + requestLength;
+            if (requestNoQuote.equals("true"))
+                strSmmry += APIRequestNoQuote;
+            if (requestBreak.equals("true"))
+                strSmmry += APIRequestBreak;
+            if (url != null)
+                strSmmry += APIRequestURL + url;
+        return strSmmry
+    
+    // Make an API call to SUMMRY and returns the response as a formatted json string.
     public static SMMRYAPI requestSummry (HashMap<String, String> parameters) throws IOException {
         String url = parameters.get("url");
         String requestLength = parameters.get("requestLength");
@@ -22,34 +40,20 @@ public class Request {
         String requestNoQuote = parameters.get("requestNoQuote");
         String requestBreak = parameters.get("requestBreak");
 
-        String strSummry = baseURL + APIKeyLink;
-        if (requestKeywordCount != null)
-            strSummry += APIRequestKeywordCount + requestKeywordCount;
-        if (requestLength != null)
-            strSummry += APIRequestLength + requestLength;
-        if (requestNoQuote.equals("true"))
-            strSummry += APIRequestNoQuote;
-        if (requestBreak.equals("true"))
-            strSummry += APIRequestBreak;
-        if (url != null)
-            strSummry += APIRequestURL + url;
-
+        strSmmry = urlBuilder()
         URL summry = new URL(strSummry);
-        System.out.println(summry);
 
         URLConnection sy = summry.openConnection();
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(sy.getInputStream()));
-
+        
         String inputLine;
         ArrayList<String> jsonArray = new ArrayList<>();
-
+            
         while ((inputLine = in.readLine()) != null) {
             jsonArray.add(inputLine);
         }
         in.close();
-
-        System.out.println(jsonArray);
 
         String json = "";
         for (String line : jsonArray)
